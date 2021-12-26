de398508bc21e075108b65383bf5ce26a1b4550c
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-12-26 21:40:35 +0600

    first

--- firstDjango/views.py
+++ firstDjango/views.py
@@ -2,8 +2,31 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def HomePage(request):
+    if request.method =="POST":
+        num1 = request.POST['number1']
+        num2 = request.POST['number2']
+        print(num1)
+        add = int(num1) + int(num2)
+        data={
+            "num1":num1,
+            "num2":num2,
+            "result": add,
+
+        }
+        return render(request,'index.html',{"fromdata":data})
     return_data = "<h1>First Django project</h1>"
     return HttpResponse(return_data)
 
+todos = [
+        {"todo": "Test Todo 1"},
+        {"todo": "Test Todo 2"},
+        {"todo": "Test Todo 3"},
+        {"todo": "Test Todo 4"},
+        {"todo": "Test Todo 5"}
+
+
+    ]
+
 def FirstPage(request):
-    return render(request,'index.html')
+    user = "Ishtiaq Ahmad Shohan"
+    return render(request,'index.html',{"data":todos})
